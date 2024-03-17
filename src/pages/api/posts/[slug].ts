9f93f7742800bda1c8ef45758da60a1d45dbc05f
Dilip Ray Ch <i@iraycd.com>
2024-03-17 17:54:31 +0530

    API Handlers removed since they are not being used

--- src/pages/api/posts/[slug].ts
+++ src/pages/api/posts/[slug].ts
@@ -2,21 +2,6 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 import clientPromise from '../../../lib/mongodb';
 
-// export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-//   try {
-
-//     const { slug } = req.query; // Access the slug from the request query
-
-//     if (!slug || typeof slug !== 'string') {
-//       res.status(400).json({ error: "Slug is required and must be a string." });
-//       return;
-//     }
-//     const data = getPostBySlugApi(slug);
-//     res.status(200).json(data);
-//   } catch (e) {
-//     res.status(500).json({ error: "Unable to connect to database: " + (e as any).message });
-//   }
-// }
 
 export async function getPostBySlugApi(slug: String) {
 
--- src/pages/api/posts/index.ts
+++ src/pages/api/posts/index.ts
@@ -2,15 +2,6 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 import clientPromise from '../../../lib/mongodb';
 
-// export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-//   try {
-//     const data = await getPostApi();
-//     res.status(200).json(data);
-//   } catch (e) {
-//     res.status(500).json({ error: "Unable to connect to database: " + (e as any).message });
-//   }
-// }
-
 export async function getPostApi() {
     const client = await clientPromise;
     const db = client.db("test");
