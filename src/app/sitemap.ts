ebfe821d9f07c73e263dc7057a2696c169f0982b
Dilip Ray Ch <i@iraycd.com>
2024-03-27 01:33:45 +0530

    Sitemap change in homepage

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -3,7 +3,7 @@ import { MetadataRoute } from 'next'
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: 'https://teja.app',
+      url: 'https://teja.app/',
       lastModified: new Date(),
       changeFrequency: 'weekly',
       priority: 1,
