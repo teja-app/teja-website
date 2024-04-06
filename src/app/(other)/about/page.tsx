143d2b05babd4f31141339dff932056ffaad5295
Dilip Ray Ch <i@iraycd.com>
2024-04-06 10:26:54 +0530

    About us added vision and mission statements

--- src/app/(other)/about/page.tsx
+++ src/app/(other)/about/page.tsx
@@ -8,20 +8,20 @@ export default function AboutPage() {
             At Teja.app, we're dedicated to making mental wellness a seamless and accessible part of everyday life through AI-driven personalization that resonates with each individual.
           </p>
           
+          {/* Vision */}
+          <section className="py-10">
+            <h2 className="text-2xl font-bold">Vision</h2>
+            <div className="flex flex-col md:flex-row justify-around items-start max-w-6xl mx-auto py-6">
+            Our vision at Teja is to make mental wellness simple and accessible for everyone. We want to create a place where you can take care of your mental health on your own, using easy-to-understand tools that help you stay calm, focused, and happy. We're here to help people look after their mental well-being early on, so they can live better, more joyful lives.
+            </div>
+          </section>
+
           {/* Mission and Values */}
           <section className="py-10">
             <h2 className="text-2xl font-bold">Our Mission</h2>
             <p className="mt-4 mx-auto max-w-md">
-              We aim to empower individuals with the tools they need to manage their emotional health, inspired by a blend of scientific insights and mindfulness practices.
+              Teja's mission is to offer straightforward, effective tools for everyday mental wellness. We're dedicated to providing a helping hand to those looking to improve their mental state through self-care, helping them find peace and clarity in their daily lives without complicating things. It's about giving everyone the chance to understand and nurture their own mental health with ease and confidence.
             </p>
-          </section>
-          
-          {/* Why Choose Us */}
-          <section className="py-10">
-            <h2 className="text-2xl font-bold">Why Choose Teja.app?</h2>
-            <div className="flex flex-col md:flex-row justify-around items-start max-w-6xl mx-auto py-6">
-              {/* Sub-sections like The Challenge, Our Vision, etc. */}
-            </div>
           </section>
           
           {/* Call to Action for Joining the Team */}
