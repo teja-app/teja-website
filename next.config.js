04ae1ae7929e0e9251f9c9dd6ed7269574d7ae72
Dilip Ray Ch <i@iraycd.com>
2024-10-17 09:05:11 +0530

    Added transpilePackages

--- next.config.js
+++ next.config.js
@@ -1,27 +1,38 @@
-const withMDX = require('@next/mdx')()
+const withMDX = require("@next/mdx")();
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'f000.backblazeb2.com',
+        protocol: "https",
+        hostname: "f000.backblazeb2.com",
       },
       {
-        protocol: 'https',
-        hostname: 'cdn.leonardo.ai',
+        protocol: "https",
+        hostname: "cdn.leonardo.ai",
       },
     ],
   },
-    pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx'],
-    webpack(config) {
-        config.module.rules.push({
-          test: /\.svg$/,
-          use: ["@svgr/webpack"]
-        });
-    
-        return config;
-      },
-}
+  pageExtensions: ["js", "jsx", "mdx", "ts", "tsx"],
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ["@svgr/webpack"],
+    });
 
-module.exports = withMDX(nextConfig)
+    return config;
+  },
+  transpilePackages: [
+    "antd",
+    "@ant-design",
+    "rc-util",
+    "rc-pagination",
+    "rc-picker",
+    "rc-notification",
+    "rc-tooltip",
+    "rc-tree",
+    "rc-table",
+  ],
+};
+
+module.exports = withMDX(nextConfig);
